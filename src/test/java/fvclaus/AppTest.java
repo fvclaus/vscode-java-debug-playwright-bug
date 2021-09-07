381ee9ae2a44c0259caac395e59227afb25ae229
clausfr <claus.hays@partner.akdb.de>
2021-09-07 20:01:40 +0200

    Initial commit

--- src/test/java/fvclaus/AppTest.java
+++ src/test/java/fvclaus/AppTest.java
@@ -1,8 +1,11 @@
 package fvclaus;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.IsEqual.equalTo;
-
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.options.Proxy;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -15,6 +18,25 @@ public class AppTest {
      */
     @Test
     public void shouldAnswerWithTrue() {
-        assertThat(true, equalTo(false));
+        try (Playwright playwright = Playwright.create()) {
+            BrowserType.LaunchOptions launchOptions = new BrowserType.LaunchOptions();
+            launchOptions.setHeadless(false);
+            String proxyEnv = System.getenv("HTTPS_PROXY");
+            if (proxyEnv != null) {
+                Proxy proxy = new Proxy(proxyEnv);
+                // proxy.setBypass(System.getenv("NO_PROXY"));
+                proxy.setPassword(System.getenv("HTTPS_PROXY_PASSWORD"));
+                proxy.setUsername(System.getenv("HTTPS_PROXY_USERNAME"));
+                System.out.println("Using proxy server " + proxy.server + ".");
+                launchOptions.setProxy(proxy);
+            }
+            Browser browser = playwright.chromium().launch(launchOptions);
+            Page page = browser.newPage();
+            page.navigate("https://playwright.dev");
+            Assert.assertTrue(
+                "Exception " + page.title() + " to contain Playwright",
+                page.title().contains("Playwright")
+            );
+        }
     }
 }
